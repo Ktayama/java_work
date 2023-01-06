d611461970911a019973504f005855fa564c5833
DESKTOP-3FLR293\NB21 <y_katayama@emyus.jp>
2023-01-06 14:01:22 +0900

    QusetionとAswerを整理いたしましたので、ご確認お願いいたします。

--- tes/src/main/java/Edit_List.java
+++ tes/src/main/java/Edit_List.java
@@ -39,56 +39,69 @@ public class Edit_List extends HttpServlet {
 		// TODO Auto-generated method stub
 		
 		
-		//Edit.jspで定義したパラメータを受け取っている
-		String param = (String)request.getAttribute("Questions");
-		String[] param_an =(String[])request.getAttribute("Answer[]");
-		String param_questions_id =(String)request.getAttribute("Questions_id");
-		String[] param_anwer_id =(String[])request.getAttribute("Answer_id[]");
-		//コンソールに問題文と答えを表示している
-		System.out.println(param);
-		System.out.println(param_an);
+		request.setCharacterEncoding("UTF-8");
+		//分岐
+		//すでにパラメータの中身は入力されている状態だからgetParameterは定義しなくてもよい
+		//getParameterでパラメータを持ってくる
+		//request.getParameter("questions_text");
+		//reques[] answer_text = request.getParameterValues("answer_text");
 		
-		String error =null;
-		
-		//配列の要素数分だけループ
-		for (int i = 0; i < param_an.length; i++) 
-		{
-			//	=をつけないと200数字も含まれる
-			// <が正しい
-			//200文字超えていればエラーに入る
-			if(param_an[i].length()>200) {
-				error="条件を超えています";
-				
+		//Edit.jspでhiddenで指定したパラメーターをgetParameterでStringedit_prm_confirm に代入している
+		String edit_prm_confirm = request.getParameter("edit_prm_confirm");
+		////submitで指定の値が押された時
+		if(edit_prm_confirm.equals("確認")) {
+			
+			
+			//Edit.jspで定義したパラメータを受け取っている
+			String param = (String)request.getParameter("questions_text");
+			String[] param_an =(String[])request.getParameterValues("answer_text");
+			String param_questions_id =(String)request.getParameter("questions_id");
+			String[] param_anwer_id =(String[])request.getParameterValues("answer_id");
+			//コンソールに問題文と答えを表示している
+			System.out.println(param);
+			System.out.println(param_an);
+			
+			String error =null;
+			
+			//配列の要素数分だけループ
+			for (int i = 0; i < param_an.length; i++) 
+			{
+				//	=をつけないと200数字も含まれる
+				// <が正しい
+				//200文字超えていればエラーに入る
+				if(param_an[i].length()>200) {
+					error="条件を超えています";
+					
+				}
 			}
+			//文字数が500文字以内かつ入力されているかつ文字が画面に存在する場合
+			if ((param.length() <= 500) && (param!="" && param!=null))
+			{
+				//文字数okならconfirm.jspに飛ばす
+				request.setAttribute("Questions",param);
+				
+				request.setAttribute("Answer[]",param_an);
+				
+				request.setAttribute("Questions_id",param_questions_id);
+				
+				request.setAttribute("Answer_id[]",param_anwer_id);
+				
+				request.setAttribute("a_text",error);
+				
+				
+				//文字数okならEdit_Confirm.jspに飛ばす
+				RequestDispatcher dispacher = request.getRequestDispatcher("./Edit_Confirm.jsp");
+				dispacher.forward(request, response);
+				//エラー文字をjqpに表示させる
+			} /* else {
+				
+				//edit.jspに戻すと思う
+				request.setAttribute("a_text",error);
+				//文字数オーバーだったらngでRsgister.jspに戻す
+				System.out.println("no");
+				RequestDispatcher dispacher = request.getRequestDispatcher("./Edit_Connection");
+				dispacher.forward(request, response);
+				}*/
 		}
-		//文字数が500文字以内かつ入力されているかつ文字が画面に存在する場合
-		if ((param.length() <= 500) && (param!="" && param!=null))
-		{
-			//文字数okならconfirm.jspに飛ばす
-			request.setAttribute("Questions",param);
-			
-			request.setAttribute("Answer[]",param_an);
-			
-			request.setAttribute("Questions_id",param_questions_id);
-			
-			request.setAttribute("Answer_id[]",param_anwer_id);
-			
-			request.setAttribute("a_text",error);
-			
-			
-			//文字数okならEdit_Confirm.jspに飛ばす
-			RequestDispatcher dispacher = request.getRequestDispatcher("./Edit_Confirm.jsp");
-			dispacher.forward(request, response);
-			//エラー文字をjqpに表示させる
-		} else {
-			
-			//edit.jspに戻すと思う
-			request.setAttribute("a_text",error);
-			//文字数オーバーだったらngでRsgister.jspに戻す
-			System.out.println("no");
-			RequestDispatcher dispacher = request.getRequestDispatcher("./Edit_Connection");
-			dispacher.forward(request, response);
-		}
-		
 	}
 }
--- tes/src/main/java/Register_Confirm_List.java
+++ tes/src/main/java/Register_Confirm_List.java
@@ -1,5 +1,3 @@
-
-
 import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
@@ -39,59 +37,72 @@ public class Register_Confirm_List extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		
+		request.setCharacterEncoding("UTF-8");
 		
-		//Questions_texパラメータを/Register_itemで取得したパラメータを受け取る
-		///Register_itemの55,57行からとっている
-		String param = request.getParameter("questions_text");
-		//getParameterValuesと書いてあるのでanswer_textの中身は[]はいらない
-		String[] param_an =request.getParameterValues("answer_text");
-		//結果をコンソールに表示
-		System.out.println(param);
-		System.out.println(param_an);
-		
-		//パラメータの文字数チェック
-		//ここでlengthを使う
-		
-		//param = new Scanner(System.in).nextLine();
-		//4つの条件が揃ったらif文の中身が実行される
-		
-		//elseの条件は4つの条件が満たなければelseに実行される
-		//一軒一軒fon文(param_an.length<=200)で取得する
-		String error =null;
-		//配列の要素数分ループ
-		for (int i = 0; i < param_an.length; i++) 
+		//分岐
+		//Register.jspでsubmitで指定したパラメータをgetParameterでString prmに代入している
+		String prm = request.getParameter("prm");
+		//Register register = new Register();
+		//submitで指定の値が押された時
+		if (prm.equals("確認"))
 		{
-			//	=をつけないと200数字も含まれる
-			// <が正しい
-			//200超えていればエラーにはいる
-			if(param_an[i].length()>200) {
-				error="条件を超えています";
-				
-				
+		
+			//Questions_texパラメータを/Register_itemで取得したパラメータを受け取る
+			///Register_itemの55,57行からとっている
+			String param = request.getParameter("questions_text");
+			//getParameterValuesと書いてあるのでanswer_textの中身は[]はいらない
+			String[] param_an =request.getParameterValues("answer_text");
+			//結果をコンソールに表示
+			System.out.println(param);
+			System.out.println(param_an);
+			
+			//パラメータの文字数チェック
+			//ここでlengthを使う
+			
+			//param = new Scanner(System.in).nextLine();
+			//4つの条件が揃ったらif文の中身が実行される
+			
+			//elseの条件は4つの条件が満たなければelseに実行される
+			//一軒一軒fon文(param_an.length<=200)で取得する
+			String error =null;
+			//配列の要素数分ループ
+			for (int i = 0; i < param_an.length; i++) 
+			{
+				//	=をつけないと200数字も含まれる
+				// <が正しい
+				//200超えていればエラーにはいる
+				if(param_an[i].length()>200) {
+					error="条件を超えています";
+					
+				}
 			}
-		}
-		//文字数が500文字以内かつ入力されているかつ文字が画面に存在する場合
-		if ((param.length() <= 500) && (param!="" && param!=null))
-		{
-			//setAttributeでパラメータをセット
-			request.setAttribute("Qusertions_text",param);
+			//文字数が500文字以内かつ入力されているかつ文字が画面に存在する場合
+			if ((param.length() <= 500) && (param!="" && param!=null))
+			{
+				//setAttributeでパラメータをセット
+				request.setAttribute("Qusertions_text",param);
+				
+				request.setAttribute("Correct_anser_text[]",param_an);
+				
+				request.setAttribute("a_text",error);
+				
+				System.out.println("ok");
+				//文字数okならregister_confirm.jspに飛ばす
+				RequestDispatcher dispacher = request.getRequestDispatcher("./register_confirm.jsp");
+				dispacher.forward(request, response);
+				//エラー文字をjqpに表示させる
+			} else {
+				request.setAttribute("a_text",error);
+				//文字数オーバーだったらngでRsgister.jspに戻す
+				System.out.println("no");
+				RequestDispatcher dispacher = request.getRequestDispatcher("./Register.jsp");
+				dispacher.forward(request, response);
+			}
 			
-			request.setAttribute("Correct_anser_text[]",param_an);
-			
-			request.setAttribute("a_text",error);
-			
-			System.out.println("ok");
-			//文字数okならregister_confirm.jspに飛ばす
-			RequestDispatcher dispacher = request.getRequestDispatcher("./register_confirm.jsp");
-			dispacher.forward(request, response);
-			//エラー文字をjqpに表示させる
-		} else {
-			request.setAttribute("a_text",error);
-			//文字数オーバーだったらngでRsgister.jspに戻す
+		}else if(prm.equals("戻る")) {
+			//確認・追加・削除以外押されたら戻るボタンに入るように表示をnoにしている
 			System.out.println("no");
-			RequestDispatcher dispacher = request.getRequestDispatcher("./register_confirm.jsp");
-			dispacher.forward(request, response);
+			request.getRequestDispatcher("./List_Connection").forward(request, response);
 		}
 	}
-
 }
